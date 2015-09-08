5f5b7dac9e5a7291e74dd2195f9a22f79d79d4d2
jrperritt <jrperritt@gmail.com>
2015-09-07 22:13:25 -0600

    shared ips extension

---
+++ rackspace/networking/v2/sharedips/requests.go
@@ -0,0 +1,230 @@
+package sharedips
+
+import (
+	"fmt"
+
+	"github.com/rackspace/gophercloud"
+	"github.com/rackspace/gophercloud/pagination"
+)
+
+// ListOptsBuilder allows extensions to add additional parameters to the
+// List request.
+type ListOptsBuilder interface {
+	ToSharedIPListQuery() (string, error)
+}
+
+// ListOpts are options for listing shared IPs. Though currently empty, having
+// it lets us add fields later should they get added to the API.
+type ListOpts struct{}
+
+// ToSharedIPListQuery formats a ListOpts into a query string.
+func (opts ListOpts) ToSharedIPListQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	if err != nil {
+		return "", err
+	}
+	return q.String(), nil
+}
+
+// List returns a Pager which allows you to iterate over a collection of
+// shared IPs.
+func List(c *gophercloud.ServiceClient, opts ListOptsBuilder) pagination.Pager {
+	url := listURL(c)
+	if opts != nil {
+		query, err := opts.ToSharedIPListQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
+	}
+
+	return pagination.NewPager(c, url, func(r pagination.PageResult) pagination.Page {
+		return IPAddressPage{pagination.SinglePageBase(r)}
+	})
+}
+
+// CreateOptsBuilder allows extensions to add additional parameters to the
+// Create request.
+type CreateOptsBuilder interface {
+	ToSharedIPCreateMap() (map[string]interface{}, error)
+}
+
+// CreateOpts contains options for provisioning a shared IP address. This object is passed to
+// the sharedips.Create function.
+type CreateOpts struct {
+	// REQUIRED
+	NetworkID string
+	// REQUIRED
+	PortIDs []string
+	// REQUIRED
+	Version int
+	// REQUIRED
+	TenantID string
+}
+
+// ToSharedIPCreateMap assembles a request body based on the contents of a
+// CreateOpts.
+func (opts CreateOpts) ToSharedIPCreateMap() (map[string]interface{}, error) {
+	v := make(map[string]interface{})
+
+	if opts.NetworkID == "" {
+		return nil, fmt.Errorf("Required CreateOpts field 'NetworkID' not set.")
+	}
+	v["network_id"] = opts.NetworkID
+
+	if len(opts.PortIDs) < 2 {
+		return nil, fmt.Errorf("Required CreateOpts field 'PortIDs' must contain at least 2 port IDs.")
+	}
+	v["port_ids"] = opts.PortIDs
+
+	if opts.Version != 4 && opts.Version != 6 {
+		return nil, fmt.Errorf("Required CreateOpts field 'Version' not set.")
+	}
+	v["version"] = opts.Version
+
+	if opts.TenantID == "" {
+		return nil, fmt.Errorf("Required CreateOpts field 'TenantID' not set.")
+	}
+	v["tenant_id"] = opts.TenantID
+
+	return map[string]interface{}{"ip_address": v}, nil
+}
+
+// Create will provision a new shapred IP address based on the values in CreateOpts. To extract
+// the IPAddress object from the response, call the Extract method on the
+// CreateResult.
+func Create(client *gophercloud.ServiceClient, opts CreateOptsBuilder) CreateResult {
+	var res CreateResult
+
+	reqBody, err := opts.ToSharedIPCreateMap()
+	if err != nil {
+		res.Err = err
+		return res
+	}
+
+	_, res.Err = client.Post(createURL(client), reqBody, &res.Body, &gophercloud.RequestOpts{
+		OkCodes: []int{201},
+	})
+	return res
+}
+
+// Delete will deallocate the existing shared IP with the provided ID from the tenant.
+// Before using this operation, all IP associations must be removed from the IP address
+// by using the Disassociate function.
+func Delete(client *gophercloud.ServiceClient, id string) DeleteResult {
+	var res DeleteResult
+	_, res.Err = client.Delete(deleteURL(client, id), nil)
+	return res
+}
+
+// Get retrieves the shared IP with the provided ID. To extract the IPAddress object
+// from the response, call the Extract method on the GetResult.
+func Get(client *gophercloud.ServiceClient, id string) GetResult {
+	var res GetResult
+	_, res.Err = client.Get(getURL(client, id), &res.Body, nil)
+	return res
+}
+
+// UpdateOptsBuilder allows extensions to add additional parameters to the
+// Update request.
+type UpdateOptsBuilder interface {
+	ToSharedIPUpdateMap() (map[string]interface{}, error)
+}
+
+// UpdateOpts contain options for updating an existing shared IP. This object is passed
+// to the sharedips.Update function.
+type UpdateOpts struct {
+	// REQUIRED
+	PortIDs []string
+}
+
+// ToSharedIPUpdateMap assembles a request body based on the contents of an
+// UpdateOpts.
+func (opts UpdateOpts) ToSharedIPUpdateMap() (map[string]interface{}, error) {
+	v := make(map[string]interface{})
+
+	if len(opts.PortIDs) < 2 {
+		return nil, fmt.Errorf("Required CreateOpts field 'PortIDs' must contain at least 2 port IDs.")
+	}
+	v["port_ids"] = opts.PortIDs
+
+	return map[string]interface{}{"ip_address": v}, nil
+}
+
+// Update will update the shared IP with provided information. To extract the updated
+// IPAddress from the response, call the Extract method on the UpdateResult.
+func Update(client *gophercloud.ServiceClient, id string, opts UpdateOptsBuilder) UpdateResult {
+	var res UpdateResult
+
+	reqBody, err := opts.ToSharedIPUpdateMap()
+	if err != nil {
+		res.Err = err
+		return res
+	}
+
+	_, res.Err = client.Put(updateURL(client, id), reqBody, &res.Body, &gophercloud.RequestOpts{
+		OkCodes: []int{200},
+	})
+	return res
+}
+
+// ListByServerOptsBuilder allows extensions to add additional parameters to the
+// ListByServer request.
+type ListByServerOptsBuilder interface {
+	ToSharedIPListByServerQuery() (string, error)
+}
+
+// ListByServerOpts are options for listing the shared IPs for a server. Though currently empty, having
+// it lets us add fields later should they get added to the API.
+type ListByServerOpts struct{}
+
+// ToSharedIPListByServerQuery formats a ListByServerOpts into a query string.
+func (opts ListOpts) ToSharedIPListByServerQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	if err != nil {
+		return "", err
+	}
+	return q.String(), nil
+}
+
+// ListByServer returns a Pager which allows you to iterate over a collection of
+// IPAssociations.
+func ListByServer(c *gophercloud.ServiceClient, serverID string, opts ListByServerOptsBuilder) pagination.Pager {
+	url := listByServerURL(c, serverID)
+	if opts != nil {
+		query, err := opts.ToSharedIPListByServerQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
+	}
+
+	return pagination.NewPager(c, url, func(r pagination.PageResult) pagination.Page {
+		return IPAssociationPage{pagination.SinglePageBase(r)}
+	})
+}
+
+// Associate will associate the server with a shared IP address.
+func Associate(client *gophercloud.ServiceClient, serverID, sharedIPID string) AssociateResult {
+	var res AssociateResult
+
+	_, res.Err = client.Post(associateURL(client, serverID, sharedIPID), nil, &res.Body, &gophercloud.RequestOpts{
+		OkCodes: []int{201},
+	})
+	return res
+}
+
+// Disassociate will delete the association between the server and the IP address.
+func Disassociate(client *gophercloud.ServiceClient, serverID, sharedIPID string) DisassociateResult {
+	var res DisassociateResult
+	_, res.Err = client.Delete(disassociateURL(client, serverID, sharedIPID), nil)
+	return res
+}
+
+// GetByServer retrieves the shared IP with the provided ID and server ID. To extract
+// the IPAssociation object from the response, call the Extract method on the GetByServerResult.
+func GetByServer(client *gophercloud.ServiceClient, serverID, sharedIPID string) GetByServerResult {
+	var res GetByServerResult
+	_, res.Err = client.Get(getByServerURL(client, serverID, sharedIPID), &res.Body, nil)
+	return res
+}
---
+++ rackspace/networking/v2/sharedips/results.go
@@ -0,0 +1,152 @@
+package sharedips
+
+import (
+	"github.com/mitchellh/mapstructure"
+	"github.com/rackspace/gophercloud"
+	"github.com/rackspace/gophercloud/pagination"
+)
+
+// IPAddress represents a shared IP address.
+type IPAddress struct {
+	ID        string   `mapstructure:"id"`
+	NetworkID string   `mapstructure:"network_id"`
+	Address   string   `mapstructure:"address"`
+	PortIDs   []string `mapstructure:"port_ids"`
+	SubnetID  string   `mapstructure:"subnet_id"`
+	TenantID  string   `mapstructure:"tenant_id"`
+	Version   int      `mapstructure:"version"`
+	Type      string   `mapstructure:"type"`
+}
+
+// IPAddressPage is the page returned by a pager when traversing over a
+// collection of shared IP addresses.
+type IPAddressPage struct {
+	pagination.SinglePageBase
+}
+
+// IsEmpty checks whether an IPAddressPage struct is empty.
+func (p IPAddressPage) IsEmpty() (bool, error) {
+	is, err := ExtractIPAddresses(p)
+	if err != nil {
+		return true, nil
+	}
+	return len(is) == 0, nil
+}
+
+// ExtractIPAddresses accepts a Page struct, specifically an IPAddressPage struct,
+// and extracts the elements into a slice of IPAddress structs. In other words,
+// a generic collection is mapped into a relevant slice.
+func ExtractIPAddresses(page pagination.Page) ([]IPAddress, error) {
+	var resp struct {
+		IPAddresses []IPAddress `mapstructure:"ip_addresses"`
+	}
+	err := mapstructure.WeakDecode(page.(IPAddressPage).Body, &resp)
+
+	return resp.IPAddresses, err
+}
+
+type commonIPAddressResult struct {
+	gophercloud.Result
+}
+
+// Extract is a function that accepts a result and extracts a shared IP resource.
+func (r commonIPAddressResult) Extract() (*IPAddress, error) {
+	if r.Err != nil {
+		return nil, r.Err
+	}
+
+	var res struct {
+		IPAddress *IPAddress `mapstructure:"ip_address"`
+	}
+
+	err := mapstructure.WeakDecode(r.Body, &res)
+
+	return res.IPAddress, err
+}
+
+// CreateResult represents the result of a create operation.
+type CreateResult struct {
+	commonIPAddressResult
+}
+
+// GetResult represents the result of a get operation.
+type GetResult struct {
+	commonIPAddressResult
+}
+
+// UpdateResult represents the result of an update operation.
+type UpdateResult struct {
+	commonIPAddressResult
+}
+
+// DeleteResult represents the result of a delete operation.
+type DeleteResult struct {
+	gophercloud.ErrResult
+}
+
+// IPAssociation represents a shared IP address for a server.
+type IPAssociation struct {
+	ID      string `mapstructure:"id"`
+	Address string `mapstructure:"address"`
+}
+
+// IPAssociationPage is the page returned by a pager when traversing over the
+// collection of shared IP addresses for a server.
+type IPAssociationPage struct {
+	pagination.SinglePageBase
+}
+
+// IsEmpty checks whether an IPAssociationPage struct is empty.
+func (p IPAssociationPage) IsEmpty() (bool, error) {
+	is, err := ExtractIPAssociations(p)
+	if err != nil {
+		return true, nil
+	}
+	return len(is) == 0, nil
+}
+
+// ExtractIPAssociations accepts a Page struct, specifically an IPAssociationPage struct,
+// and extracts the elements into a slice of IPAssociation structs.
+func ExtractIPAssociations(page pagination.Page) ([]IPAssociation, error) {
+	var resp struct {
+		IPAssociations []IPAssociation `mapstructure:"ip_associations"`
+	}
+
+	err := mapstructure.Decode(page.(IPAssociationPage).Body, &resp)
+
+	return resp.IPAssociations, err
+}
+
+type commonIPAssociationResult struct {
+	gophercloud.Result
+}
+
+// Extract is a function that accepts a result and extracts a shared IP association.
+func (r commonIPAssociationResult) Extract() (*IPAssociation, error) {
+	if r.Err != nil {
+		return nil, r.Err
+	}
+
+	var res struct {
+		IPAssociation *IPAssociation `mapstructure:"ip_association"`
+	}
+
+	err := mapstructure.Decode(r.Body, &res)
+
+	return res.IPAssociation, err
+}
+
+// AssociateResult represents the result of an Associate operation.
+type AssociateResult struct {
+	commonIPAssociationResult
+}
+
+// GetByServerResult represents the result of a GetByServer operation.
+type GetByServerResult struct {
+	commonIPAssociationResult
+}
+
+// DisassociateResult represents the result of a Disassociate operation.
+type DisassociateResult struct {
+	gophercloud.ErrResult
+}
