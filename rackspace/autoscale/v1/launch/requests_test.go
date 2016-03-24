66ce2e774bb4dd697770afafcfcb34eeb80d2403
Brad Ison <bison@garbagebrain.org>
2016-03-24 19:21:59 -0400

    Rackspace Auto Scale: Add lanuch config Get()

---
+++ rackspace/autoscale/v1/launch/requests_test.go
@@ -0,0 +1,25 @@
+package launch
+
+import (
+	"testing"
+
+	th "github.com/rackspace/gophercloud/testhelper"
+	"github.com/rackspace/gophercloud/testhelper/client"
+)
+
+func TestGetLaunch(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	HandleLanuchGetSuccessfully(t)
+
+	client := client.ServiceClient()
+	groupID := "10eb3219-1b12-4b34-b1e4-e10ee4f24c65"
+
+	config, err := Get(client, groupID).Extract()
+
+	if err != nil {
+		t.Fatalf("Unexpected Get error: %v", err)
+	}
+
+	th.CheckDeepEquals(t, LaunchConfig, *config)
+}
