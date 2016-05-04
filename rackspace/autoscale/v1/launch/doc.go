62c51fcfbd75b586f41484914639ac95bc0c1e03
Brad Ison <bison@garbagebrain.org>
2016-05-04 16:43:07 -0500

    Rackspace Auto Scale: Add lanuch config docs

---
+++ rackspace/autoscale/v1/launch/doc.go
@@ -0,0 +1,10 @@
+/*
+Package launch provides information and interaction with the launch
+configuration API resource in the Rackspace Auto Scale service.
+
+Auto Scale uses launch configurations to describe the attributes of servers in
+scaling groups.  Details include from which image to create a server, which
+cloud load balancers to join servers to, which networks to add servers to, and
+other metadata.
+*/
+package launch
