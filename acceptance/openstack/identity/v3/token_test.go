2d63c4a7307fbbe31caf28661b56dc06e76402fe
Joe Topjian <joe@topjian.net>
2016-05-13 05:03:58 +0000

    Fixing v3 token auth
    This commit fixes identity v3 token authentication by looking at
    the correct struct for the token. An acceptance test has been
    added to verify token authentication works.

--- acceptance/openstack/identity/v3/token_test.go
+++ acceptance/openstack/identity/v3/token_test.go
@@ -5,6 +5,7 @@ package v3
 import (
 	"testing"
 
+	"github.com/rackspace/gophercloud"
 	"github.com/rackspace/gophercloud/openstack"
 	tokens3 "github.com/rackspace/gophercloud/openstack/identity/v3/tokens"
 )
@@ -39,4 +40,58 @@ func TestGetToken(t *testing.T) {
 	}
 
 	t.Logf("Acquired token: %s", token.ID)
+}
+
+func TestTokenAuth(t *testing.T) {
+	// Create a service client.
+	ao, err := openstack.AuthOptionsFromEnv()
+	if err != nil {
+		t.Fatalf("Unable to acquire credentials: %v", err)
+	}
+
+	// Save the tenant name
+	tenantName := ao.TenantName
+
+	// Trim out unused fields.
+	ao.TenantID, ao.TenantName = "", ""
+
+	if ao.DomainID == "" {
+		ao.DomainID = "default"
+	}
+
+	providerClient, err := openstack.AuthenticatedClient(ao)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	// Create a service client.
+	serviceClient := openstack.NewIdentityV3(providerClient)
+
+	// Get a token
+	scope := &tokens3.Scope{
+		ProjectName: tenantName,
+		DomainID:    ao.DomainID,
+	}
+
+	result := tokens3.Create(serviceClient, ao, scope)
+	token, err := result.ExtractToken()
+	if err != nil {
+		t.Fatalf("Could not generate a token: %v", err)
+	}
+
+	// Now create a new session with the token
+	newAO := gophercloud.AuthOptions{
+		TokenID:          token.ID,
+		IdentityEndpoint: ao.IdentityEndpoint,
+	}
+
+	newProviderClient, err := openstack.AuthenticatedClient(newAO)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+
+	newClient := openstack.NewIdentityV3(newProviderClient)
+	if newClient == nil {
+		t.Fatalf("Could not create a client based on a token")
+	}
 }
