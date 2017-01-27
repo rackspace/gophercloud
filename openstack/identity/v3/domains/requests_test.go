246abad19def0be2453411efbf5bd6271fb2b7bb
Christopher Parratto <christopher_parratto@comcast.com>
2017-01-27 17:21:44 -0500

    Added paginated index for domains endpoint.

---
+++ openstack/identity/v3/domains/requests_test.go
@@ -0,0 +1,91 @@
+package domains
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/rackspace/gophercloud/pagination"
+	"github.com/rackspace/gophercloud/testhelper"
+	"github.com/rackspace/gophercloud/testhelper/client"
+)
+
+func TestListSinglePage(t *testing.T) {
+	testhelper.SetupHTTP()
+	defer testhelper.TeardownHTTP()
+
+	testhelper.Mux.HandleFunc("/domains", func(w http.ResponseWriter, r *http.Request) {
+		testhelper.TestMethod(t, r, "GET")
+		testhelper.TestHeader(t, r, "X-Auth-Token", client.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, `
+			{
+                "domains": [
+                    {
+                        "description": "Used for swift functional testing",
+                        "enabled": true,
+                        "id": "5a75994a383c449184053ff7270c4e91",
+                        "links": {
+                            "self": "http://example.com/identity/v3/domains/5a75994a383c449184053ff7270c4e91"
+                        },
+                        "name": "swift_test"
+                    },
+                    {
+                        "description": "Owns users and tenants (i.e. projects) available on Identity API v2.",
+                        "enabled": true,
+                        "id": "default",
+                        "links": {
+                            "self": "http://example.com/identity/v3/domains/default"
+                        },
+                        "name": "Default"
+                    }
+                ],
+                "links": {
+                    "next": null,
+                    "previous": null,
+                    "self": "http://example.com/identity/v3/domains"
+                }
+            }
+		`)
+	})
+
+	count := 0
+	err := List(client.ServiceClient(), ListOpts{}).EachPage(func(page pagination.Page) (bool, error) {
+		count++
+		actual, err := ExtractDomains(page)
+		if err != nil {
+			return false, err
+		}
+
+		expected := []Domain{
+			Domain{
+				Description: "Used for swift functional testing",
+				ID:          "5a75994a383c449184053ff7270c4e91",
+				Name:        "swift_test",
+				Enabled:     true,
+				Links:       Link{Self: "http://example.com/identity/v3/domains/5a75994a383c449184053ff7270c4e91"},
+			},
+			Domain{
+				Description: "Owns users and tenants (i.e. projects) available on Identity API v2.",
+				ID:          "default",
+				Name:        "Default",
+				Enabled:     true,
+				Links:       Link{Self: "http://example.com/identity/v3/domains/default"},
+			},
+		}
+
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("Expected %#v, got %#v", expected, actual)
+		}
+
+		return true, nil
+	})
+	if err != nil {
+		t.Errorf("Unexpected error while paging: %v", err)
+	}
+	if count != 1 {
+		t.Errorf("Expected 1 page, got %d", count)
+	}
+}
---
+++ openstack/identity/v3/domains/results.go
@@ -0,0 +1,50 @@
+package domains
+
+import (
+	"github.com/rackspace/gophercloud"
+	"github.com/rackspace/gophercloud/pagination"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+type commonResult struct {
+	gophercloud.Result
+}
+
+// Link the object to hold a project link.
+type Link struct {
+	Self string `json:"self,omitempty"`
+}
+
+// Domian is main struct for holding domain attributes.
+type Domain struct {
+	Description string `json:"description,omitempty"`
+	Enabled     bool   `json:"enabled"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Links       Link   `json:"self"`
+}
+
+// DomainPage is a single page of Domain results.
+type DomainPage struct {
+	pagination.LinkedPageBase
+}
+
+// IsEmpty returns true if the page contains no results.
+func (p DomainPage) IsEmpty() (bool, error) {
+	domains, err := ExtractDomains(p)
+	if err != nil {
+		return true, err
+	}
+	return len(domains) == 0, nil
+}
+
+// ExtractDomains extracts a slice of Domains from a Collection acquired from List.
+func ExtractDomains(page pagination.Page) ([]Domain, error) {
+	var response struct {
+		Domains []Domain `mapstructure:"domains"`
+	}
+
+	err := mapstructure.Decode(page.(DomainPage).Body, &response)
+	return response.Domains, err
+}
